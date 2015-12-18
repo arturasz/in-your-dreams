9b3ecb224c9fd3c1f0324582ce0a5ef1f75c5741
Arturas Zakrevskis <arturasz@wix.com>
2015-12-18 14:16:54 +0200

    restored description

--- components/Home.js
+++ components/Home.js
@@ -138,7 +138,7 @@ class Home extends React.Component {
                    />
                 <View style={styles.textPlaceholder}>
                   <Text style={styles.title}>{this.state.dreams[0].title}</Text>
-                  <Text style={styles.description}>iLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laboru</Text>
+                  <Text style={styles.description}>{this.state.dreams[0].description}</Text>
                 </View>
               </View>
               <View style={styles.buttonContainer}>
